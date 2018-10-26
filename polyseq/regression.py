17e2a5ed69c4cc33aaa3a370ebb1111477b40f08
Jason Wittenbach <jason.wittenbach@gmail.com>
2018-10-25 20:51:58 -0400

    updates regression to handle single-dimensional regressors

--- polyseq/regression.py
+++ polyseq/regression.py
@@ -7,7 +7,7 @@ from polyseq.expression_matrix import ExpressionMatrix
 def regress(data, regressors, n_processes=1):
 
     regressors = np.array(regressors)
-    regressors = data[regressors] if regressors.ndim == 1 else regressors
+    regressors = regressors[:, np.newaxis] if regressors.ndim == 1 else regressors
 
     model = LinearRegression(n_jobs=n_processes)
     model.fit(regressors, data)
