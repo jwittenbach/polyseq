2cfde7ef798e72e744e682bfb2a2ad4356da7ba9
Jason Wittenbach <jason.wittenbach@gmail.com>
2018-12-02 19:56:52 -0500

    adds loading of cellranger hdf5 files

--- polyseq/io.py
+++ polyseq/io.py
@@ -3,6 +3,7 @@ from subprocess import call
 
 import pandas as pd
 from scipy.io import mmread
+import tables
 
 from polyseq.expression_matrix import ExpressionMatrix
 
@@ -14,12 +15,47 @@ def read_mtx(exp_matrix_path, genes=None):
     return ExpressionMatrix(exp_matrix)._finalize()
 
 def read_cellranger(path):
+    if path[-3:] == ".h5":
+        expression_matrix = _load_cellranger_h5(path)
+    else:
+        expression_matrix = _load_cellranger_mtx(path)
+    return expression_matrix
+
+def _load_cellranger_h5(path):
+    '''
+    https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/advanced/h5_matrices
+    '''
+    with tables.open_file(path, 'r') as f:
+        mat_group = list(f.iter_nodes(f.root))[0] # assuming first and only group is the correct one
+        print("reading data")
+        data = mat_group.data.read()
+        print(data.dtype)
+        print("reading indices")
+        indices = mat_group.indices.read()
+        print(indices.dtype)
+        print("reading indptrs")
+        indptr = mat_group.indptr.read()
+        print(indptr.dtype)
+        print("reading shape")
+        shape = mat_group.shape.read()
+        print(shape.dtype)
+        print(shape)
+        print("reading genes")
+        genes = mat_group.gene_names.read()
+    print("making sparse matrix")
+    arr = csc_matrix((data, indices, indptr), shape=shape)#.toarray().T
+    print("making dense matrix")
+    return arr.toarray().T
+    exp_matrix = pd.DataFrame(arr).rename(genes, axis=1)
+    return ExpressionMatrix(exp_matrix)
+
+def _load_cellranger_mtx(path):
     genes = pd.read_csv(path + "/genes.tsv", delimiter='\t', header=None)[1]
     try:
         expression_matrix = read_mtx(path + "/matrix.mtx", genes=genes)
     except:
         expression_matrix = read_mtx(path + "/matrix.mtx.gz", genes=genes)
-    return expression_matrix._finalize()
+    return expression_matrix
 
 def read_pickle(path):
     return ExpressionMatrix(pd.read_pickle(path))
